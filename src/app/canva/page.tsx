128497ba22782ca2a82ee7da34706b02d13157b7
moozart-tech <mauroserralvo@gmail.com>
2025-06-28 14:39:11 +0200

    popup update

--- src/app/canva/page.tsx
+++ src/app/canva/page.tsx
@@ -1,38 +1,42 @@
 "use client"
 import Canvas from '@/components/canvas'
+import { useEffect, useState } from 'react'
+import { HelveticaBold } from "@/lib/fonts"
 
 export default function Page() {
-  // const [showBetaWarning, setShowBetaWarning] = useState(false)
+  const [showBetaWarning, setShowBetaWarning] = useState(false)
 
-  // useEffect(() => {
-    // Verificar si el usuario ya ha visto el aviso
-    // const hasSeenBetaWarning = localStorage.getItem('hasSeenBetaWarning')
-    // if (!hasSeenBetaWarning) {
-    //     setShowBetaWarning(true)
-    //     localStorage.setItem('hasSeenBetaWarning', 'true')
-    //   }
-    // }, [])
+  useEffect(() => {
+    const hasSeenBetaWarning = localStorage.getItem('hasSeenBetaWarning')
+    if (!hasSeenBetaWarning) {
+        setShowBetaWarning(true)
+        localStorage.setItem('hasSeenBetaWarning', 'true')
+    }
+  }, [])
 
   return (
     <main className='fixed inset-0 '>
-      {/* {showBetaWarning && (
+      {showBetaWarning && (
         <div className="fixed inset-0 bg-neutral-500/40 z-[1000] flex items-center justify-center">
-          <div className="bg-white/90 border border-black/15 p-12 rounded-lg max-w-lg mx-4">
-            <div className={font.className}>
-              <h3 className="text-4xl mb-3">We&apos;re in Beta</h3>
+          <div className="bg-white/90 border border-black/15 p-12 max-w-2xl mx-4 w-full">
+            <div className={HelveticaBold.className}>
+              <h3 className="text-3xl mb-4 uppercase">AI Features Temporarily Disabled</h3>
             </div>
-            <p className="text-neutral-600 text-md mb-4">
-              Brinpage Board is currently in development. AI and file upload functions will be available soon.
+            <p className="text-neutral-600 text-sm mb-4">
+              AI generation functions are disabled due to API costs. If you want to test it in depth you can clone the repository locally and run it with your key.
             </p>
             <button 
               onClick={() => setShowBetaWarning(false)}
-              className="bg-blue-600 text-white hover:text-white hover:bg-blue-600 px-4 py-2 mt-1 rounded-lg w-full transition-colors cursor-pointer"
+              className="bg-transparent text-blue-600 hover:underline cursor-pointer pt-3  inline-flex items-center gap-x-2"
             >
               Continue
+              <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M9 18l6-6-6-6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
             </button>
           </div>
         </div>
-      )} */}
+      )}
       <Canvas />
     </main>
   )
